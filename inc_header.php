2cd5e9537c1f8970435f798aefd51ec77ec8485a
Andreas Scheil <git@scheil.eu>
2017-02-13 17:45:10 +0100

    Init

---
+++ inc_header.php
@@ -0,0 +1,20 @@
+<?php 
+/**
+Autoren: Henrik Lewecke, Andreas Scheil
+Erstelldatum: 01.04.2016
+
+Beschreibung: Header, wird jeder sichtbaren PHP Seite hinzugefügt, Einbinden der Externen Dateien, Sessionhandling
+
+**/
+?>
+<!DOCTYPE html> 
+<html>
+	<head>
+		<title>fancyMCU</title>
+		<meta charset="UTF-8">
+		<link rel="stylesheet" href="css/bootstrap.min.css">
+		<link rel="stylesheet" href="css/bootstrap-theme.css">
+		<script src="js/jquery.js"></script>
+		<script src="js/jquery-ui.min.js"></script>		
+	</head>
+	<body>
